5e1181457b9951c78bc4a348ecf6d67612f3bcae
“vmurarishetty” <vmurarishetty@personagraph.com>
2018-03-05 19:08:20 +0530

    pokemon pow

--- assets/app.js
+++ assets/app.js
@@ -59,6 +59,20 @@ App = {
       // Set the provider for our contract
       App.contracts.PowZone.setProvider(App.web3Provider);
       
+      return App.initPokemonPowContract(finalCallBack);
+      
+    });
+  },
+
+  initPokemonPowContract: function(finalCallBack) {
+    $.getJSON('./assets/PokemonPow.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var pokemonPowTokenArtifact = data;
+      App.contracts.PokemonPow = TruffleContract(pokemonPowTokenArtifact);
+
+      // Set the provider for our contract
+      App.contracts.PokemonPow.setProvider(App.web3Provider);
+      
       if(typeof finalCallBack !== 'undefined'){
         return finalCallBack();
       }
